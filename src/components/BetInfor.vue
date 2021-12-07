8b12632e7492e15ad65a2571241fd93a074cbad4
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2021-12-07 17:51:07 +0800

    //重寫統計資訊頁面

--- src/components/BetInfor.vue
+++ src/components/BetInfor.vue
@@ -1,16 +1,28 @@
 <template>
 	<div class="row betInfo-container">
     	<ul class="col-6 text-md-left">
-        <li class="font_red">莊家 : {{betInfo.banker}}</li>    
-        <li class="font_yellow">閒家 : {{betInfo.player}}</li> 
-        <li class="font_green">和局 : {{betInfo.tie}}</li> 
-        <li class="font_red">莊對 : {{betInfo.bankerPair}}</li> 
-        <li class="font_yellow">閒對 : {{betInfo.playerPair}}</li> 
-        <li v-if="totalRound>0">總局數 : {{totalRound}}</li>
-        <li v-else>總局數 : 0</li>
+        <section v-if="betInfo">
+          <li class="font_red">莊家 : {{betInfo.banker}}</li>    
+          <li class="font_yellow">閒家 : {{betInfo.player}}</li> 
+          <li class="font_green">和局 : {{betInfo.tie}}</li> 
+          <li class="font_red">莊對 : {{betInfo.bankerPair}}</li> 
+          <li class="font_yellow">閒對 : {{betInfo.playerPair}}</li> 
+          <li v-if="totalRound>0">總局數 : {{totalRound}}</li>
+          <li v-else>總局數 : 0</li>
+        </section>
+        <section v-else>
+          <li class="font_red">莊家 : 0</li>    
+          <li class="font_yellow">閒家 : 0</li> 
+          <li class="font_green">和局 : 0</li> 
+          <li class="font_red">莊對 : 0</li> 
+          <li class="font_yellow">閒對 : 0</li> 
+          <li v-if="totalRound>0">總局數 : 0</li>
+          <li v-else>總局數 : 0</li>
+        </section>
+        
       </ul>
-      <ul class="col-6 text-md-right" v-if="betInfo">
-        <section v-if="totalRound>0">
+      <ul class="col-6 text-md-right">
+        <section v-if="totalRound>0 && betInfo">
           <li class="font_red">{{Math.floor((betInfo.banker/totalRound)*100)}}%</li>    
           <li class="font_yellow">{{Math.floor((betInfo.player/totalRound)*100)}}%</li> 
           <li class="font_green">{{Math.floor((betInfo.tie/totalRound)*100)}}%</li> 
