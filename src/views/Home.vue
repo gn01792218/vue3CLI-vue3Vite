79b5ed53e03250a885a38912f9b85fdce1bcd5a8
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-02-21 13:07:52 +0800

    新增Home 桌卡牌RWD

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1 class="chooseTable text-center">選擇遊戲桌檯</h1>
     <div
-      class="tableList flex-column flex-xl-row justify-content-center align-items-center mt-5 mt-xl-0 h-100"
+      class="tableList mt-2 mr-auto ml-auto"
     >
       <TableInfoCard
         v-for="(i, index) in tableDataList"
